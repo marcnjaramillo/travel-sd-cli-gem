6f03832482d405eb9213fb87fa7cd4e9df916806
marcnjaramillo <marcn.jaramillo@gmail.com>
2016-10-06 00:49:29 -0700

    Add method for finding destination by id, clean up some code in the scrape method

--- lib/travel_sd/destination.rb
+++ lib/travel_sd/destination.rb
@@ -1,5 +1,5 @@
 class TravelSd::Destination
-  attr_accessor :name
+  attr_accessor :name, :special
 
   def self.first
     self.first_list
@@ -11,6 +11,10 @@ class TravelSd::Destination
 
   def self.third
     self.third_list
+  end
+
+  def self.find(id)
+    self.first[id-1]
   end
 
   def self.first_list
@@ -33,27 +37,31 @@ class TravelSd::Destination
 
   def self.scrape_first
     doc = Nokogiri::HTML(open("http://hotels.sandiego.org/attraction/list/246/m24?page=1"))
+    place = doc.search("h2[itemprop='name']")
+    deal = doc.search("div.aresDealTextInner p")
 
     destination = self.new
-    destination.name = doc.search("h2[itemprop='name']").text.strip
-
+    destination.name = place.map(&:text).join("\n")
+    destination.special = deal.map(&:text).join("\n")
     destination
   end
 
   def self.scrape_second
     doc = Nokogiri::HTML(open("http://hotels.sandiego.org/attraction/list/246/m24?page=2"))
+    place = doc.search("h2[itemprop='name']")
 
     destination = self.new
-    destination.name = doc.search("h2[itemprop='name']").text.strip
+    destination.name = place.map(&:text).join("\n")
 
     destination
   end
 
   def self.scrape_third
     doc = Nokogiri::HTML(open("http://hotels.sandiego.org/attraction/list/246/m24?page=3"))
+    place = doc.search("h2[itemprop='name']")
 
     destination = self.new
-    destination.name = doc.search("h2[itemprop='name']").text.strip
+    destination.name = place.map(&:text).join("\n")
 
     destination
   end
