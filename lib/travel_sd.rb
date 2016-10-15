6dad370ea70f56470e7bc2f074cb4115c98086a0
marcnjaramillo <marcn.jaramillo@gmail.com>
2016-10-15 12:29:59 -0700

    Add require_relative for new scraper.rb file

--- lib/travel_sd.rb
+++ lib/travel_sd.rb
@@ -3,5 +3,6 @@ require "pry"
 require "nokogiri"
 
 require_relative "./travel_sd/version"
-require_relative "./travel_sd/cli"
+require_relative "./travel_sd/scraper"
 require_relative "./travel_sd/destination"
+require_relative "./travel_sd/cli"
